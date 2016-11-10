888e1e2e3b85af29a9e9d12e3bdfa873b5adc9b6
Ariel Tay <shihqian@gmail.com>
2016-11-10 22:09:40 +0800

    first commit of react-kit, with react-bootstrap

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navigation from './Navigation.jsx'
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Navigation />
+      </div>
+    )
+  }
+}
+
+export default App
